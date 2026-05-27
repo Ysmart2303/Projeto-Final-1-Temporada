06b243cb1a232e2193eedf5a178c88c872042bab
Ysmart2303 <viniciussaraiva3004@gmail.com>
2026-05-27 12:33:04 -0300

    tentando funcionar #2

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ function voltar(index) {
         home: "index.html",
         creditos: "creditos.html",
         login : "login.html",
-        
+        esCo : "escolhaCurso.html"
     };
 
     window.location.href = paginas[index];
